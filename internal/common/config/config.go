12f88a90cf5ad623097abe3001fbdfe3fcdf3954
Ilya Samsonov <ixlax@mail.ru>
2026-09-10 14:56:59 +0300

    feat: add project structure and config loader

---
+++ internal/common/config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import "os"
+
+type Config struct {
+	ServerPort string
+	DbHost     string
+	DbPort     string
+	DbName     string
+	DbUser     string
+	DbPassword string
+}
+
+func Load() *Config {
+
+	return &Config{
+
+		ServerPort: os.Getenv("SERVER_PORT"),
+		DbHost:     os.Getenv("POSTGRES_HOST"),
+		DbPort:     os.Getenv("POSTGRES_PORT"),
+		DbName:     os.Getenv("POSTGRES_DB"),
+		DbUser:     os.Getenv("POSTGRES_USER"),
+		DbPassword: os.Getenv("POSTGRES_PASSWORD"),
+	}
+
+}
